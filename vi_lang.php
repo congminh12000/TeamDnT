80d0c354cda63322bf32491193c6417f243b02ed
thinh <thinh@gmail.com>
2017-04-23 00:47:10 +0700

    upload language

---
+++ vi_lang.php
@@ -0,0 +1,11 @@
+<?php
+$lang = array();
+$lang['title'] = 'Trang Chủ';
+$lang['introduction'] = 'Giới Thiệu';
+$lang['service'] = 'Dịch Vụ';
+$lang['contact'] = 'Liên Hệ';
+$lang['devweb'] = 'Thiết Kế Website';
+$lang['devtrade'] = 'Thiết Kế Bộ Nhận Dạng Thương Hiệu';
+$lang['savedata'] = 'Lưu Trữ Dữ Liệu';
+?>
+
