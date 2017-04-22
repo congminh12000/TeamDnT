5b969369562aeeda207f09fa930e56702a579fa4
thinh <thinh@gmail.com>
2017-04-23 00:45:54 +0700

    complete multi langague

--- index_hotnews.php
+++ index_hotnews.php
@@ -1,13 +1,24 @@
+
+
 <div class="hotnews">
-	<div class="container">
-    	<div class="row">
-        	<div class="col-xs-5 col-sm-5 col-md-5 col-lg-5 text-left">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-5 col-sm-5 col-md-5 col-lg-5 text-left">
+                <form name="lang_f" method="post">
+                    <input  accept=""type="hidden" name="lg">
+                    <td  align="center">
+                      
+                        <a href = '?lang=vi'><img src = 'images/logo_vi.jpg' width = '50px' height = '25px' border = '0' style = "padding-top: 7px; padding-left: 25px;" alt = 'Vietnamese'></a>
+                        <a href = '?lang=en'><img src = 'images/logo_en.jpg' width = '50px' height = '25px' border = '0' style = "padding-top: 7px; padding-left: 25px" alt = 'English'></a>
+    
+                    </td>
+                </form>
             </div>
             <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 text-right">
             </div>
-    		<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1 text-right hotnews_box">
-            	<a href="<?php echo $url ?>dntgate.html" target="_blank"><i class="fa fa-lock" aria-hidden="true"></i></a>
+            <div class="col-xs-1 col-sm-1 col-md-1 col-lg-1 text-right hotnews_box">
+                <a href="<?php echo $url ?>dntgate.html" target="_blank"><i class="fa fa-lock" aria-hidden="true"></i></a>
             </div>
-      	</div>  
+        </div>  
     </div>
 </div> <!-- End hotnews -->
