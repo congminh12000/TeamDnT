d028f6cc2c963e43c867e63ef4907bbd537c2f87
thinh <thinh@gmail.com>
2017-04-23 00:47:04 +0700

    upload language

---
+++ en_lang.php
@@ -0,0 +1,11 @@
+<?php
+$lang = array();
+$lang['title'] = 'Home';
+$lang['introduction'] = 'Introduction';
+$lang['service'] = 'Service';
+$lang['contact'] = 'Contact';
+$lang['devweb'] = 'Design Website';
+$lang['devtrade'] = 'Thiết Kế Bộ Nhận Dạng Thương Hiệu';
+$lang['savedata'] = 'Lưu Trữ Dữ Liệu';
+?>
+
